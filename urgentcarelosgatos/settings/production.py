1b61ca85cce6c025fcfcde436abb82caedba98d3
observateur1980 <44187905+observateur1980@users.noreply.github.com>
2019-06-03 00:49:27 -0700

    asa

--- urgentcarelosgatos/settings/production.py
+++ urgentcarelosgatos/settings/production.py
@@ -2,5 +2,5 @@
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 DEBUG = False
-ALLOWED_HOSTS = ['www.urgentcarelosgatos.com', 'urgentcarelosgatos.com']
+ALLOWED_HOSTS = ['173.255.219.149','www.urgentcarelosgatos.com', 'urgentcarelosgatos.com']
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
